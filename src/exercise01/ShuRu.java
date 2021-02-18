f87f5eee0b444c5326a73f3f032ebc0d0aeae22f
GongYuu <gongyu0227@gamil.com>
2021-02-18 16:54:34 +0800

    第一天练习02
    02

---
+++ src/exercise01/ShuRu.java
@@ -0,0 +1,17 @@
+package exercise01;
+
+import java.util.Scanner;
+
+public class ShuRu {
+    public static void main(String[] args) {
+        //步骤1  导包
+
+        //步骤2  创建对象
+        Scanner sc = new Scanner(System.in);
+        //步骤3  使用变量接收数据
+        int i =sc.nextInt();
+
+        //步骤4  打印
+        System.out.println(i);
+    }
+}
